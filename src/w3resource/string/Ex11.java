f5720387b63f7a9bfa6ee37057d7f7ec058ae347
Ross Khapilov <rossxpanteley@gmail.com>
2017-12-04 16:45:37 +0300

    Commit 36

---
+++ src/w3resource/string/Ex11.java
@@ -0,0 +1,13 @@
+package w3resource.string;
+
+/**
+ * @author Ross Khapilov
+ * @version 1.0 created on 03.12.2017
+ */
+public class Ex11{
+    public static void main(String[] args) {
+        char[] ch = {'a','w','b'};
+        String str = new String(ch);
+        System.out.println(str);
+    }
+}
---
+++ src/w3resource/string/Ex13.java
@@ -0,0 +1,13 @@
+package w3resource.string;
+
+/**
+ * @author Ross Khapilov
+ * @version 1.0 created on 03.12.2017
+ */
+public class Ex13 {
+    public static void main(String[] args) {
+        String s1 = "sd";
+        String s2 = "sd";
+        System.out.println(s1.equals(s2));
+    }
+}
