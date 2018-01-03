270383978fc68db7458c21415e2e4aaf89a68641
Ross Khapilov <rossxpanteley@gmail.com>
2018-01-03 23:32:55 +0300

    Commit 54

--- src/w3resource/number/Ex2.java
+++ src/w3resource/number/Ex2.java
@@ -2,7 +2,7 @@ package w3resource.number;
 
 /**
  * @author Ross Khapilov
- * @version 1.0 created on 02.01.2018
+ * @version 2.0 created on 02.01.2018
  */
 public class Ex2 {
     public static boolean isAbundant(int num) {
@@ -12,9 +12,33 @@ public class Ex2 {
         }
         return num < divisors;
     }
+    public static boolean isPerfectNumber(int num) {
+        int divisors = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) divisors += i;
+        }
+        return num == divisors;
+    }
+    public static boolean isDeficientNumber(int num) {
+        int divisors = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) divisors += i;
+        }
+        return num > divisors;
+    }
 
     public static void main(String[] args) {
-        System.out.println(isAbundant(12));
-        System.out.println(isAbundant(11));
+        int countAbundant = 0;
+        int countPerfect = 0;
+        int countDeficient = 0;
+        for (int i = 1; i < 10_000; i++) {
+            if(isAbundant(i)) countAbundant++;
+            if(isPerfectNumber(i)) countPerfect++;
+            if(isDeficientNumber(i)) countDeficient++;
+        }
+        System.out.println("Abundant: " + countAbundant);
+        System.out.println("Perfect number: " + countPerfect);
+        System.out.println("Deficient : " + countDeficient);
+
     }
 }
