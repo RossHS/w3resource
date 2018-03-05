24c1229f495c73c66ca34d5656344ca71e0302c4
Ross Khapilov <rossxpanteley@gmail.com>
2018-03-05 21:53:23 +0300

    Commit 80

---
+++ src/w3resource/Collection/LL25.java
@@ -0,0 +1,15 @@
+package w3resource.Collection;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * @author Ross Khapilov
+ * @version 1.0 created on 05.03.2018
+ */
+public class LL25 {
+    public static void main(String[] args) {
+        LinkedList<Integer> a = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println(a.isEmpty());
+    }
+}
